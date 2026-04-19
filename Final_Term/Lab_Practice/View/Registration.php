bb63688a8abb4f89abee0a026d48a69ae9e4952a
Shihab Khan <22-49326-3@student.aiub.edu>
2026-04-19 08:47:10 -0500

    Add Lab Practice

---
+++ Final_Term/Lab_Practice/View/Registration.php
@@ -0,0 +1,17 @@
+<?php
+    include "../Controller/RegistrationController.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Page</title>
+</head>
+<body>
+    <h1>welcome to Registration Page</h1>
+    <form method="POST" action=""></form>
+    
+</body>
+</html>
