3c11f778c4cf6d16613f34a17e1457b957842592
Shihab Khan <22-49326-3@student.aiub.edu>
2026-05-06 16:03:48 -0500

    add Login Page

--- Final_Term/UserFeedbackSystem/View/Login.php
+++ Final_Term/UserFeedbackSystem/View/Login.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    <h1>This is login page</h1>
+
+    <h2>Login</h2>
+    
+    <form action="../Controller/LoginController.php" method="POST">
+        <div>
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username" required>
+        </div>   
+        <br>
+        <div>
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <br>
+
+        <button type="submit" name="login">Login</button>
+    </form>
+
+    <p>Don't have an account? <a href="Registration.php">Register here</a></p>
+
 </body>
 </html>
